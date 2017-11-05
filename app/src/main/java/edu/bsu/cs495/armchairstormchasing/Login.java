5ceaf7d5ac97440ea45280e6bda3dca68ca45b52
Dylon <dprice@bsu.edu>
2017-11-05 09:46:39 -0500

    Initial commit

---
+++ app/src/main/java/edu/bsu/cs495/armchairstormchasing/Login.java
@@ -0,0 +1,13 @@
+package edu.bsu.cs495.armchairstormchasing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
