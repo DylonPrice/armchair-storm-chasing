5f24c69c56477b436c2728b5f5135661c1e6d2f4
David Wisenberg <dmwisenberg@bsu.edu>
2018-03-18 19:21:40 -0400

    Added navigation drawer feature

--- app/src/main/java/edu/bsu/cs495/armchairstormchasing/MapActivity.java
+++ app/src/main/java/edu/bsu/cs495/armchairstormchasing/MapActivity.java
@@ -2,6 +2,8 @@ package edu.bsu.cs495.armchairstormchasing;
 
 import android.content.Context;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -16,6 +18,9 @@ import org.osmdroid.views.overlay.MapEventsOverlay;
 import org.osmdroid.views.overlay.Marker;
 
 public class MapActivity extends AppCompatActivity {
+
+    private DrawerLayout mainDrawerLayout;
+    private ActionBarDrawerToggle mainToggle;
 
     @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +63,8 @@ public class MapActivity extends AppCompatActivity {
 
         MapEventsOverlay OverlayEvents = new MapEventsOverlay(getBaseContext(), mReceive);
         map.getOverlays().add(OverlayEvents);
+
+        setUpNavigationDrawer();
     }
 
     public void onResume(){
@@ -67,5 +74,13 @@ public class MapActivity extends AppCompatActivity {
         //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         //Configuration.getInstance().save(this, prefs);
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+    }
+
+    public void setUpNavigationDrawer(){
+        mainDrawerLayout = (DrawerLayout) findViewById(R.id.mapActivity);
+        mainToggle = new ActionBarDrawerToggle(this, mainDrawerLayout, R.string.open, R.string.close);
+        mainDrawerLayout.addDrawerListener(mainToggle);
+        mainToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
